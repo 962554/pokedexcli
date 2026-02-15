2f84afb3ee4111eb664782ba33061eae924c7761
962554 <254557023+962554@users.noreply.github.com>
2026-02-15 11:23:04 +0000

    Update tests
    1. rename struct: cases -> testCases
    2. add 'name' to testCases struct
    3. use t.Run
    4. simplify Errorf messages
    
    ---------------------------------
    1) Why is the change being made?
    ---------------------------------
    
    2) What has changed?
    ---------------------------------
    
    3) Other concerns and/or dependent changes? (if exist)
    ---------------------------------
    
    4) Related sprint issue, TT, and/or MCM (if exist)
    ---------------------------------
    
    5) Related design/durability review/ORR document link (if exist)
    ---------------------------------
    
    6) Testing (if exist)
    ---------------------------------

--- repl_test.go
+++ repl_test.go
@@ -1,42 +1,50 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCleanInput(t *testing.T) {
-	cases := []struct {
+	testCases := []struct {
+		name     string
 		input    string
 		expected []string
 	}{
 		{
+			name:     "pre-lc string",
 			input:    " hello world ",
 			expected: []string{"hello", "world"},
 		},
 		{
+			name:     "empty",
 			input:    " ",
 			expected: []string{},
 		},
 		{
+			name:     "single-word",
 			input:    " hello ",
 			expected: []string{"hello"},
 		},
 		{
+			name:     "capatalised words",
 			input:    " Hello World ",
 			expected: []string{"hello", "world"},
 		},
 	}
 
-	for _, c := range cases {
-		actual := cleanInput(c.input)
-		if len(actual) != len(c.expected) {
-			t.Errorf("Test fails: slice lengths don't match: %d != %d\n", len(actual), len(c.expected))
-			continue
-		}
-		for i := range actual {
-			word := actual[i]
-			expectedWord := c.expected[i]
-			if expectedWord != word {
-				t.Errorf("cleanInput(%v): Test fails: %s != %s\n", c.input, word, expectedWord)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := cleanInput(tc.input)
+			if len(got) != len(tc.expected) {
+				t.Errorf("slice lengths don't match: expected: %d, got: %d", len(tc.expected), len(got))
 			}
-		}
+			for i := range got {
+				word := got[i]
+				expectedWord := tc.expected[i]
+				if expectedWord != word {
+					t.Errorf("input: %q, expected: %s, got: %s", tc.input, expectedWord, word)
+				}
+			}
+		})
 	}
 }
