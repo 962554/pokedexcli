d2770a81b107ef3de6cbaba667b7503e35373dbd
962554 <254557023+962554@users.noreply.github.com>
2026-02-20 04:10:27 +0000

    feat: add new package: pokecache

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,65 @@
+package pokecache
+
+import (
+	_ "fmt"
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	cache    map[string]cacheEntry
+	mu       sync.Mutex
+	interval time.Duration
+}
+
+func NewCache(interval time.Duration) *Cache {
+	cache := &Cache{}
+	cache.cache = make(map[string]cacheEntry)
+	cache.interval = interval
+
+	go func() {
+		ticker := time.NewTicker(interval)
+		for range ticker.C {
+			cache.reapLoop()
+		}
+	}()
+
+	return cache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.cache[key] = cacheEntry{
+		val:       val,
+		createdAt: time.Now(),
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if entry, ok := c.cache[key]; ok {
+		return entry.val, ok
+	}
+	return nil, false
+}
+
+func (c *Cache) reapLoop() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	interval := c.interval
+	for key, entry := range c.cache {
+		if time.Since(entry.createdAt).Seconds() > float64(interval.Seconds()) {
+			delete(c.cache, key)
+		}
+	}
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
