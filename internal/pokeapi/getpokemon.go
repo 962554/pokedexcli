3000e8d730cd7900fceb6f043141bde237a860e2
962554 <254557023+962554@users.noreply.github.com>
2026-02-22 13:23:33 +0000

    feat: add new function pokeapi function: GetPokemon
    This will be used for the new `catch` command.
    
    ---------------------------------
    1) Why is the change being made?
    ---------------------------------
    
    2) What has changed?
    ---------------------------------
    
    3) Other concerns and/or dependent changes? (if exist)
    ---------------------------------
    
    4) Related sprint issue, TT, and/or MCM (if exist)
    ---------------------------------
    
    5) Related design/durability review/ORR document link (if exist)
    ---------------------------------
    
    6) Testing (if exist)
    ---------------------------------

---
+++ internal/pokeapi/getpokemon.go
@@ -0,0 +1,378 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// Pokemon represents detailed information about a specific Pokemon from the PokeAPI.
+type Pokemon struct {
+	// Abilities is a list of abilities this Pokemon could potentially have.
+	Abilities []struct {
+		Ability struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"ability"`
+		IsHidden bool `json:"is_hidden"`
+		Slot     int  `json:"slot"`
+	} `json:"abilities"`
+	// BaseExperience is the base experience gained for defeating this Pokemon.
+	BaseExperience int `json:"base_experience"`
+	// Cries contains the URLs for the latest and legacy cries of this Pokemon.
+	Cries          struct {
+		Latest string `json:"latest"`
+		Legacy string `json:"legacy"`
+	} `json:"cries"`
+	// Forms is a list of forms this Pokemon can take.
+	Forms []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"forms"`
+	// GameIndices is a list of game indices relevant to Pokemon item by generation.
+	GameIndices []struct {
+		GameIndex int `json:"game_index"`
+		Version   struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"version"`
+	} `json:"game_indices"`
+	// Height is the height of this Pokemon in decimetres.
+	Height    int `json:"height"`
+	// HeldItems is a list of items this Pokemon may be holding when encountered.
+	HeldItems []struct {
+		Item struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"item"`
+		VersionDetails []struct {
+			Rarity  int `json:"rarity"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"held_items"`
+	// ID is the identifier for this Pokemon resource.
+	ID                     int    `json:"id"`
+	// IsDefault is true for the default form of this Pokemon.
+	IsDefault              bool   `json:"is_default"`
+	// LocationAreaEncounters is the URL for the location area encounters for this Pokemon.
+	LocationAreaEncounters string `json:"location_area_encounters"`
+	// Moves is a list of moves that can be learned by this Pokemon.
+	Moves                  []struct {
+		Move struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"move"`
+		VersionGroupDetails []struct {
+			LevelLearnedAt  int `json:"level_learned_at"`
+			MoveLearnMethod struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"move_learn_method"`
+			Order        int `json:"order"`
+			VersionGroup struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version_group"`
+		} `json:"version_group_details"`
+	} `json:"moves"`
+	// Name is the name of this Pokemon.
+	Name          string `json:"name"`
+	// Order is the order for sorting. Almost national order, except families and forms are together.
+	Order         int    `json:"order"`
+	// PastAbilities is a list of abilities this Pokemon has had in previous generations.
+	PastAbilities []struct {
+		Abilities []struct {
+			Ability  any  `json:"ability"`
+			IsHidden bool `json:"is_hidden"`
+			Slot     int  `json:"slot"`
+		} `json:"abilities"`
+		Generation struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"generation"`
+	} `json:"past_abilities"`
+	// PastStats is a list of stats this Pokemon has had in previous generations.
+	PastStats []struct {
+		Generation struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"generation"`
+		Stats []struct {
+			BaseStat int `json:"base_stat"`
+			Effort   int `json:"effort"`
+			Stat     struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"stat"`
+		} `json:"stats"`
+	} `json:"past_stats"`
+	// PastTypes is a list of types this Pokemon has had in previous generations.
+	PastTypes []struct {
+		Generation struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"generation"`
+		Types []struct {
+			Slot int `json:"slot"`
+			Type struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"type"`
+		} `json:"types"`
+	} `json:"past_types"`
+	// Species is the species this Pokemon belongs to.
+	Species struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"species"`
+	// Sprites is a set of images used to depict this Pokemon in the game.
+	Sprites struct {
+		BackDefault      string `json:"back_default"`
+		BackFemale       any    `json:"back_female"`
+		BackShiny        string `json:"back_shiny"`
+		BackShinyFemale  any    `json:"back_shiny_female"`
+		FrontDefault     string `json:"front_default"`
+		FrontFemale      any    `json:"front_female"`
+		FrontShiny       string `json:"front_shiny"`
+		FrontShinyFemale any    `json:"front_shiny_female"`
+		Other            struct {
+			DreamWorld struct {
+				FrontDefault string `json:"front_default"`
+				FrontFemale  any    `json:"front_female"`
+			} `json:"dream_world"`
+			Home struct {
+				FrontDefault     string `json:"front_default"`
+				FrontFemale      any    `json:"front_female"`
+				FrontShiny       string `json:"front_shiny"`
+				FrontShinyFemale any    `json:"front_shiny_female"`
+			} `json:"home"`
+			OfficialArtwork struct {
+				FrontDefault string `json:"front_default"`
+				FrontShiny   string `json:"front_shiny"`
+			} `json:"official-artwork"`
+			Showdown struct {
+				BackDefault      string `json:"back_default"`
+				BackFemale       any    `json:"back_female"`
+				BackShiny        string `json:"back_shiny"`
+				BackShinyFemale  any    `json:"back_shiny_female"`
+				FrontDefault     string `json:"front_default"`
+				FrontFemale      any    `json:"front_female"`
+				FrontShiny       string `json:"front_shiny"`
+				FrontShinyFemale any    `json:"front_shiny_female"`
+			} `json:"showdown"`
+		} `json:"other"`
+		Versions struct {
+			GenerationI struct {
+				RedBlue struct {
+					BackDefault      string `json:"back_default"`
+					BackGray         string `json:"back_gray"`
+					BackTransparent  string `json:"back_transparent"`
+					FrontDefault     string `json:"front_default"`
+					FrontGray        string `json:"front_gray"`
+					FrontTransparent string `json:"front_transparent"`
+				} `json:"red-blue"`
+				Yellow struct {
+					BackDefault      string `json:"back_default"`
+					BackGray         string `json:"back_gray"`
+					BackTransparent  string `json:"back_transparent"`
+					FrontDefault     string `json:"front_default"`
+					FrontGray        string `json:"front_gray"`
+					FrontTransparent string `json:"front_transparent"`
+				} `json:"yellow"`
+			} `json:"generation-i"`
+			GenerationIi struct {
+				Crystal struct {
+					BackDefault           string `json:"back_default"`
+					BackShiny             string `json:"back_shiny"`
+					BackShinyTransparent  string `json:"back_shiny_transparent"`
+					BackTransparent       string `json:"back_transparent"`
+					FrontDefault          string `json:"front_default"`
+					FrontShiny            string `json:"front_shiny"`
+					FrontShinyTransparent string `json:"front_shiny_transparent"`
+					FrontTransparent      string `json:"front_transparent"`
+				} `json:"crystal"`
+				Gold struct {
+					BackDefault      string `json:"back_default"`
+					BackShiny        string `json:"back_shiny"`
+					FrontDefault     string `json:"front_default"`
+					FrontShiny       string `json:"front_shiny"`
+					FrontTransparent string `json:"front_transparent"`
+				} `json:"gold"`
+				Silver struct {
+					BackDefault      string `json:"back_default"`
+					BackShiny        string `json:"back_shiny"`
+					FrontDefault     string `json:"front_default"`
+					FrontShiny       string `json:"front_shiny"`
+					FrontTransparent string `json:"front_transparent"`
+				} `json:"silver"`
+			} `json:"generation-ii"`
+			GenerationIii struct {
+				Emerald struct {
+					FrontDefault string `json:"front_default"`
+					FrontShiny   string `json:"front_shiny"`
+				} `json:"emerald"`
+				FireredLeafgreen struct {
+					BackDefault  string `json:"back_default"`
+					BackShiny    string `json:"back_shiny"`
+					FrontDefault string `json:"front_default"`
+					FrontShiny   string `json:"front_shiny"`
+				} `json:"firered-leafgreen"`
+				RubySapphire struct {
+					BackDefault  string `json:"back_default"`
+					BackShiny    string `json:"back_shiny"`
+					FrontDefault string `json:"front_default"`
+					FrontShiny   string `json:"front_shiny"`
+				} `json:"ruby-sapphire"`
+			} `json:"generation-iii"`
+			GenerationIv struct {
+				DiamondPearl struct {
+					BackDefault      string `json:"back_default"`
+					BackFemale       any    `json:"back_female"`
+					BackShiny        string `json:"back_shiny"`
+					BackShinyFemale  any    `json:"back_shiny_female"`
+					FrontDefault     string `json:"front_default"`
+					FrontFemale      any    `json:"front_female"`
+					FrontShiny       string `json:"front_shiny"`
+					FrontShinyFemale any    `json:"front_shiny_female"`
+				} `json:"diamond-pearl"`
+				HeartgoldSoulsilver struct {
+					BackDefault      string `json:"back_default"`
+					BackFemale       any    `json:"back_female"`
+					BackShiny        string `json:"back_shiny"`
+					BackShinyFemale  any    `json:"back_shiny_female"`
+					FrontDefault     string `json:"front_default"`
+					FrontFemale      any    `json:"front_female"`
+					FrontShiny       string `json:"front_shiny"`
+					FrontShinyFemale any    `json:"front_shiny_female"`
+				} `json:"heartgold-soulsilver"`
+				Platinum struct {
+					BackDefault      string `json:"back_default"`
+					BackFemale       any    `json:"back_female"`
+					BackShiny        string `json:"back_shiny"`
+					BackShinyFemale  any    `json:"back_shiny_female"`
+					FrontDefault     string `json:"front_default"`
+					FrontFemale      any    `json:"front_female"`
+					FrontShiny       string `json:"front_shiny"`
+					FrontShinyFemale any    `json:"front_shiny_female"`
+				} `json:"platinum"`
+			} `json:"generation-iv"`
+			GenerationIx struct {
+				ScarletViolet struct {
+					FrontDefault string `json:"front_default"`
+					FrontFemale  any    `json:"front_female"`
+				} `json:"scarlet-violet"`
+			} `json:"generation-ix"`
+			GenerationV struct {
+				BlackWhite struct {
+					Animated struct {
+						BackDefault      string `json:"back_default"`
+						BackFemale       any    `json:"back_female"`
+						BackShiny        string `json:"back_shiny"`
+						BackShinyFemale  any    `json:"back_shiny_female"`
+						FrontDefault     string `json:"front_default"`
+						FrontFemale      any    `json:"front_female"`
+						FrontShiny       string `json:"front_shiny"`
+						FrontShinyFemale any    `json:"front_shiny_female"`
+					} `json:"animated"`
+					BackDefault      string `json:"back_default"`
+					BackFemale       any    `json:"back_female"`
+					BackShiny        string `json:"back_shiny"`
+					BackShinyFemale  any    `json:"back_shiny_female"`
+					FrontDefault     string `json:"front_default"`
+					FrontFemale      any    `json:"front_female"`
+					FrontShiny       string `json:"front_shiny"`
+					FrontShinyFemale any    `json:"front_shiny_female"`
+				} `json:"black-white"`
+			} `json:"generation-v"`
+			GenerationVi struct {
+				OmegarubyAlphasapphire struct {
+					FrontDefault     string `json:"front_default"`
+					FrontFemale      any    `json:"front_female"`
+					FrontShiny       string `json:"front_shiny"`
+					FrontShinyFemale any    `json:"front_shiny_female"`
+				} `json:"omegaruby-alphasapphire"`
+				XY struct {
+					FrontDefault     string `json:"front_default"`
+					FrontFemale      any    `json:"front_female"`
+					FrontShiny       string `json:"front_shiny"`
+					FrontShinyFemale any    `json:"front_shiny_female"`
+				} `json:"x-y"`
+			} `json:"generation-vi"`
+			GenerationVii struct {
+				Icons struct {
+					FrontDefault string `json:"front_default"`
+					FrontFemale  any    `json:"front_female"`
+				} `json:"icons"`
+				UltraSunUltraMoon struct {
+					FrontDefault     string `json:"front_default"`
+					FrontFemale      any    `json:"front_female"`
+					FrontShiny       string `json:"front_shiny"`
+					FrontShinyFemale any    `json:"front_shiny_female"`
+				} `json:"ultra-sun-ultra-moon"`
+			} `json:"generation-vii"`
+			GenerationViii struct {
+				BrilliantDiamondShiningPearl struct {
+					FrontDefault string `json:"front_default"`
+					FrontFemale  any    `json:"front_female"`
+				} `json:"brilliant-diamond-shining-pearl"`
+				Icons struct {
+					FrontDefault string `json:"front_default"`
+					FrontFemale  any    `json:"front_female"`
+				} `json:"icons"`
+			} `json:"generation-viii"`
+		} `json:"versions"`
+	} `json:"sprites"`
+	// Stats is a list of base stats for this Pokemon.
+	Stats []struct {
+		BaseStat int `json:"base_stat"`
+		Effort   int `json:"effort"`
+		Stat     struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"stat"`
+	} `json:"stats"`
+	// Types is a list of types this Pokemon has.
+	Types []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"type"`
+	} `json:"types"`
+	// Weight is the weight of this Pokemon in hectograms.
+	Weight int `json:"weight"`
+}
+
+// GetPokemon retrieves information about a specific Pokemon by name or ID.
+// It first checks the cache, and if not found, it makes an HTTP GET request to the PokeAPI.
+func GetPokemon(pokemon string) (Pokemon, error) {
+	url := PokemonEndpoint + pokemon
+	var data []byte
+	data, ok := cache.Get(url)
+	if !ok {
+		res, err := http.Get(url)
+		if err != nil {
+			return Pokemon{}, fmt.Errorf("http.Get failed: %w", err)
+		}
+
+		defer res.Body.Close()
+
+		data, err = io.ReadAll(res.Body)
+		if err != nil {
+			return Pokemon{}, fmt.Errorf("ioutil.ReadAll failed: %w", err)
+		}
+		cache.Add(url, data)
+	}
+
+	var resource Pokemon
+
+	if err := json.Unmarshal(data, &resource); err != nil {
+		return Pokemon{}, fmt.Errorf("json.Unmarshal failed: %w", err)
+	}
+
+	return resource, nil
+}
